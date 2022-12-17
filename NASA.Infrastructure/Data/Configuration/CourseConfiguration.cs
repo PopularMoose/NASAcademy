585ecde7d75f2172100757b60a14b49c26303f1c
Stanislav Genov <stasigenov456@gmail.com>
2022-12-17 21:00:45 +0200

    lll

--- NASA.Infrastructure/Data/Configuration/CourseConfiguration.cs
+++ NASA.Infrastructure/Data/Configuration/CourseConfiguration.cs
@@ -31,7 +31,6 @@ namespace NASA.Infrastructure.Data.Configuration
                 {
                     Id = 2,
                     Title = "Math for Beginners",
-
                     Description = "Course 'Math for Beginners' gives people basic understanding of math needed for further specialization in NASAcademy.The course includes" +
                       "'Rational Numbers','IRational Numbers','Real Numbers','Prime Numbers','Composite Numbers' and many more",
                     ImageUrl =  "https://assets.nautil.us/sites/3/nautilus/Landau_BREAKER.png?auto=compress&fm=png",
@@ -44,7 +43,6 @@ namespace NASA.Infrastructure.Data.Configuration
                 {
                     Id = 3,
                     Title = "Biology for Beginners",
-
                     Description = "Course 'Biology for Beginners' gives people basic understanding of biology needed for further specialization in NASAcademy.The course includes" +
                       "'Life','Cells','Evolution','Genes','Homeostasis' and many more",
                     ImageUrl =  "https://as1.ftcdn.net/v2/jpg/04/07/37/74/1000_F_407377427_cAi21m47R3TLy1vulqo0W7P9aWV9eiQ7.jpg",
@@ -57,7 +55,6 @@ namespace NASA.Infrastructure.Data.Configuration
                 {
                     Id = 4,
                     Title = "Engineering Introduction",
-
                     Description = "'Engineering Introduction' provides basic understanding of " +
                       "'Mechatronics','Electronics','Robot Systems','Software Engineering','Chemical and Biotechnoly Engineering' and many more",
                     ImageUrl =  "https://c1.wallpaperflare.com/preview/457/57/127/technical-drawing-calipers-workshop-mechanical-engineering.jpg",
@@ -69,7 +66,6 @@ namespace NASA.Infrastructure.Data.Configuration
                 {
                     Id = 5,
                     Title = "Aviation Introduction",
-
                     Description = "'Aviation Introduction' provides basic understanding of " +
                       "'Principles of Flight','Airframe and Systems','Airline Operations','Air Traffic Services','Airline & Airport Marketing Management' and many more",
                     ImageUrl= "https://assets.oneweb.net/s3fs-public/styles/hero_xl_fallback/public/2022-01/2.4_aviation_hero.jpg?itok=Da1Na7Ar",
@@ -84,7 +80,6 @@ namespace NASA.Infrastructure.Data.Configuration
                     Title = "Practise with Air-crafts",
                     Description = "'Practise with Air-crafts' physical trainning and understanding of the air-craft model " ,
                      ImageUrl = "https://st2.depositphotos.com/1741969/6592/i/600/depositphotos_65926649-stock-photo-close-up-photo-of-womans.jpg",
-
                     PricePerCourse = 1000.00M,
                     CategoryId = 3,
                     TeacherId = 1
